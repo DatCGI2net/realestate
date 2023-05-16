bb3e0065afb6603d7cf689aa22bf2ffef44ca539
Dat <github@cgito.net>
2023-05-16 22:11:03 +0700

    initial

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Pilot",
+
+    'summary': """
+        An realestate advertisement""",
+
+    'description': """
+        Real Estate
+    """,
+
+    'author': "Hip Dev",
+    'website': "http://www.cgito.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales/CRM',
+    'version': '16.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'account',
+
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+
+}
